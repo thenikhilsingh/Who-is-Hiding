e8e2f7d18838791a6a62bd950d4be8b41302a0bc
thenikhilsingh <nikhilsingh26042004@gmail.com>
2026-06-23 19:48:04 +0530

    created the function which detects the position where user clicks on the image

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,4 +1,5 @@
 import { ArrowLeft, Clock } from "lucide-react";
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const levels = {
@@ -75,6 +76,34 @@ export default function Game() {
 
   const level = levels[levelId];
 
+  const [clickPosition, setClickPosition] = useState(null);
+  const handleImageClick = (e) => {
+    // console.log(e); //Browser ek event object deta hai.
+    // console.log(e.clientX); // Browser window ke left se itna px door click hua
+    // console.log(e.clientY); //Browser window ke  top se itna px niche click hua
+    const ImgDimensions = e.currentTarget.getBoundingClientRect();
+    //ImgDimensions output:-
+    // {
+    //  left:100, //Image browser ke px se start ho rahi hai
+    //  top:50, //Image browser ke px top se start ho rahi hai
+    //  width:1200, //Image browser ke
+    //  height:700 //Image browser ke
+    // }
+    // console.log(ImgDimensions);
+    const relativeX = e.clientX - ImgDimensions.left; //Image ke left se px par click hua
+    const relativeY = e.clientY - ImgDimensions.top; //Image ke top se px par click hua
+
+    //Abhi Bhi Problem Hai Maan lo meri screen: 1920px  aur Tumhari screen:1366px hai to 550px dono screens par same position nahi hogi. Game logic break jayega.
+    // Solution → Percentage
+    const x = (relativeX / ImgDimensions.width) * 100;
+    const y = (relativeY / ImgDimensions.height) * 100;
+    // console.log(x, y);
+    setClickPosition({
+      x,
+      y,
+    });
+  };
+
   return (
     <div className="min-h-screen bg-[url('/BG.png')] bg-cover bg-center text-white">
       <div className="min-h-screen bg-black/50 p-6">
@@ -98,14 +127,48 @@ export default function Game() {
 
         <div className="max-w-7xl mx-auto grid lg:grid-cols-[1fr_320px] gap-6">
           {/* Main Image */}
-          <div className="bg-slate-900/70 border border-slate-700 rounded-3xl p-4">
+          <div className="relative bg-slate-900/70 border border-slate-700 rounded-3xl p-4">
             <h2 className="text-3xl font-bold mb-4">{level.title}</h2>
 
             <img
               src={level.image}
               alt={level.title}
               className="w-full rounded-2xl"
+              onClick={handleImageClick}
             />
+            {clickPosition && (
+              <div
+                className="absolute w-60 bg-[#071124] border border-cyan-500 rounded-2xl overflow-hidden shadow-2xl z-50"
+                style={{
+                  left: `${clickPosition.x}%`,
+                  top: `${clickPosition.y}%`,
+                  transform: "translate(-50%, -50%)",
+                }}
+              >
+                <div className="px-4 py-3 border-b border-slate-700 font-bold text-cyan-400">
+                  Who did you find?
+                </div>
+
+                {level.characters.map((character) => (
+                  <button
+                    key={character.id}
+                    className="flex items-center gap-3 w-full px-4 py-3 hover:bg-slate-800 transition"
+                    onClick={() => {
+                      console.log(character.name);
+                      setClickPosition(null);
+                    }}
+                  >
+                    <img
+                      src={character.image}
+                      alt={character.name}
+                      className="w-12 h-12 object-contain"
+                    />
+
+                    <span>{character.name}</span>
+                  </button>
+                ))}
+              </div>
+            )}
           </div>
 
           {/* Character Panel */}
